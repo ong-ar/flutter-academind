151526eca5c12021e122f092e00000c567167538
ong-ar <reclusis@gmail.com>
2019-09-14 14:58:31 +0900

    stateful 적용 및 확인

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   int questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex++;
+    });
     print(questionIndex);
   }
 
